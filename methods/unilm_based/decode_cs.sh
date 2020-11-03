7ed634d97aba4c5b9748771e4c929f6c469f41ea
Ubuntu <ubuntu@ip-172-31-72-38.ec2.internal>
2020-11-03 15:53:10 +0000

    decode sh

--- methods/unilm_based/decode_cs.sh
+++ methods/unilm_based/decode_cs.sh
@@ -5,13 +5,13 @@ MODEL_RECOVER_PATH="tmp/finetuned_models/bert_save/model.${EPOCH}.bin"
 EVAL_SPLIT=$2
 
 python unilm/src/biunilm/decode_seq2seq.py --bert_model bert-large-cased --new_segment_ids --mode s2s --need_score_traces \
---cs_input ${DATA_DIR}/commongen.${EVAL_SPLIT}.cs_rel.txt --input_file ${DATA_DIR}/commongen.${EVAL_SPLIT}.src_alpha.txt --split ${EVAL_SPLIT} \
---model_recover_path ${MODEL_RECOVER_PATH} \
+--cs_input ${DATA_DIR}/commongen.${EVAL_SPLIT}.cs_rel.txt --exp_file ${DATA_DIR}/commongen.${EVAL_SPLIT}.exp.txt \
+--input_file ${DATA_DIR}/commongen.${EVAL_SPLIT}.src_alpha.txt --split ${EVAL_SPLIT} --model_recover_path ${MODEL_RECOVER_PATH} \
 --max_seq_length 64 --max_tgt_length 32 \
 --batch_size 1 --beam_size 5 --length_penalty 0 \
 --forbid_duplicate_ngrams --forbid_ignore_word "."
 
 
-cp tmp/finetuned_models/bert_save/model.${EPOCH}.bin.${EVAL_SPLIT} decoded_sentences/${EVAL_SPLIT}
+cp tmp/finetuned_models/bert_save/model.${EPOCH}.bin.${EVAL_SPLIT} decoded_sentences/${EVAL_SPLIT}.${EPOCH}
 
 
