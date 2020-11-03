ddc50fef793ea4aa7e5a19622c93bcd0ff6dfebd
Ubuntu <ubuntu@ip-172-31-72-38.ec2.internal>
2020-11-03 15:01:50 +0000

    install scripts

---
+++ evaluation/scripts/install_pivot.sh
@@ -0,0 +1,6 @@
+yes|conda create -n pivot_score python=3.6
+source ~/anaconda3/etc/profile.d/conda.sh
+yes|conda activate pivot_score
+yes|pip install spacy
+yes|python -m spacy download en
+yes|pip install networkx
