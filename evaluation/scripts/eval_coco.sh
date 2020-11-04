ed4f822d70a826675d5e3c24fa8da155b9806af8
Ubuntu <ubuntu@ip-172-31-91-135.ec2.internal>
2020-11-04 06:03:19 +0000

    eval scripts

---
+++ evaluation/scripts/eval_coco.sh
@@ -0,0 +1,9 @@
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate coco_score
+TYPE=test
+INPUT_FILE=~/CommonsenseReasoning/dataset/final_data/commongen/commongen.${TYPE}.src_alpha.txt
+TRUTH_FILE=~/CommonsenseReasoning/dataset/final_data/commongen/commongen.${TYPE}.tgt.txt
+PRED_FILE=~/CommonsenseReasoning/methods/unilm_based/decoded_sentences/${TYPE}
+
+cd ~/CommonsenseReasoning/evaluation/Traditional/eval_metrics
+~/anaconda3/envs/unilm_env/bin/python eval.py --key_file ${INPUT_FILE} --gts_file ${TRUTH_FILE} --res_file ${PRED_FILE} >> ~/results.txt
